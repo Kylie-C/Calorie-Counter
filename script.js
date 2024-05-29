fafc2ccc18a6d486a7e93eee8fe3764d0fd5484e
Kylie <kylie.conner93@gmail.com>
2024-05-29 10:40:59 -0400

    Got the #budget and #entry-dropdown elements and assigned them to variables.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const calorieCounter = document.getElementById("calorie-counter");
-// To access an HTML element with a given ID name, I use the getElementById() method. In the HTML, the ID for 
-// the form is "calorie-counter", so that's what needs to be coppied into the parenthesis (as seen in orange).
-// I stored this data in the calorieCounter variable.
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+// Got my #budget element (# = ID) and #entry-dropdown and assigned them to the above variables.
