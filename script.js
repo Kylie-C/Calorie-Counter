47566f92f3f0bc780c6f5c263e1bdfa92244247c
Kylie <kylie.conner93@gmail.com>
2024-05-29 10:37:55 -0400

    Created a calorieCounter variable to access the form ID in HTML.

---
+++ script.js
@@ -0,0 +1,4 @@
+const calorieCounter = document.getElementById("calorie-counter");
+// To access an HTML element with a given ID name, I use the getElementById() method. In the HTML, the ID for 
+// the form is "calorie-counter", so that's what needs to be coppied into the parenthesis (as seen in orange).
+// I stored this data in the calorieCounter variable.
