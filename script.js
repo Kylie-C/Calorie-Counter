9a9710c9469be0742c1049cf0570e33766ceef3d
Kylie <kylie.conner93@gmail.com>
2024-05-31 14:16:11 -0400

    Created function cleanInputString (str).

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
-// In programming, prefixing a variable with IS or HAS is a common practice to signify that the variable
-// represents a boolean value (true = on, false = off). Some examples:
-// let isRunning = true;          boolean is on
-// let hasCompleted = false;      boolean is off
+function cleanInputString (str){
+}
 
-// I created the boolean variable isError and set it = false. Using LET means the variable can be reassigned 
-// later on.     Later in this project, I'll update the value of isError is the user provides an invalid
-// input.
+// When users input their daily calorie budget, the input field will only accept numerical values. 
+// However, if a number is entered with a + or - sign, I'll need to remove those characters. So, I declared
+// a cleanInputString function that takes a str parameter.
+
+// I know str = string, but I'm pretty sure 
+// it's not a js thing, but a variable thing? And I haven't created the variable yet? I'll learn as we go on.
+
+// NOTE: Values from an HTML input field are received as strings in js. I'll need to convert these strings into
+// numbers before performing any calculations. Converting string values into numbers will be covered in a 
+// future step.
