d511927527506a999f4b07d0ada9819a8f528f85
Kylie <kylie.conner93@gmail.com>
2024-05-31 16:04:48 -0400

    Changed the pattern in the regex variable.

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString (str){
-    const regex = /hello/;
+    const regex = /\+-/;
 }
 
-// To match specific characters in a string, you can use Regular Expressions or "regex" for short (Notice the
-// color of it is darker than const or let variables).
-// Regex in JS is indicated by a pattern wrapped in forward slashes. The example above matches the 
-// string literal "hello".
-// In future steps, I'll update this regex pattern to match specific characters needed for the calorie
-// counter.
+// The former /hello/ matched the exact text "hello", which was not the desired behavior. Instead, I want 
+// to search for +, -, or spaces. I replaced the /hello/ in my regex variable with \+- to match plus and 
+// minus characters.
+
+// NOTE: I needed to use the backslash \ character to escape the + symbol because it has a special meaning
+// in regular expressions.
