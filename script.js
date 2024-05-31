286241e05e8128d97f62ae4a26e5535bfacfa289
Kylie <kylie.conner93@gmail.com>
2024-05-31 14:00:14 -0400

    Added 3 more variables to js.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
-// Got my #budget element (# = ID) and #entry-dropdown and assigned them to the above variables.
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+
+// Added 3 more variables. The document.getElementById() links the html ID's to the js, so the orange in '' 
+// are the ID names in the html.
