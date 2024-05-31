f8ad6b3eb4a4e92ea0dee39c3cc746333aca6275
Kylie <kylie.conner93@gmail.com>
2024-05-31 16:12:22 -0400

    Added \s to the regex pattern.

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString (str){
-    const regex = /\+-/;
+    const regex = /\+-\s/;
 }
 
-// The former /hello/ matched the exact text "hello", which was not the desired behavior. Instead, I want 
-// to search for +, -, or spaces. I replaced the /hello/ in my regex variable with \+- to match plus and 
-// minus characters.
-
-// NOTE: I needed to use the backslash \ character to escape the + symbol because it has a special meaning
-// in regular expressions.
+// In regex, shorthand character classes allow you to match specific characters without having to write those
+// characters in your pattern. Shorthand character classes are preceded with a backslash \. NOTE that 
+// the other backslash (\+) is used to escape something (the computer's meaning of +). 
+// The character class \s will match any whitespace character, so I added this to my regex pattern.
