dd26ff57d3feb3e6b9e1daa89726849f8a34170d
Kylie <kylie.conner93@gmail.com>
2024-06-03 09:34:30 -0400

    Removed the console.log's

--- script.js
+++ script.js
@@ -7,16 +7,7 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) { 
-    console.log("original string: ", str);
     const regex = /[+-\s]/g; 
     return str.replace(regex, "");
   }
-
-  console.log(cleanInputString("+-99"))
-//  To see the results from the cleanInputString function, I needed to add a console.log() statement outside
-// of th function. Inside that console statement, I called the cleanInputString function with the string value
-// of "+-99" as an argument. By placing the "+-99" in place of the "str" in the console.log() statement,
-// I'm telling the computer that "+-99" IS str, which is why +-99 replaces all instances of str when it's
-// being run. 
-// When I open the console, I see the first console.log() show "original string: +-99" and the second
-// console.log() show "99", with the +- removed.
+// Since I finished testing the cleanInputString(){} function, I removed the console.log's.
