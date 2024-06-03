0ba937253f67c4251b4ea6b073285a2388e8f75a
Kylie <kylie.conner93@gmail.com>
2024-06-03 15:43:29 -0400

    Returned the result of calling the .match() method on str and passed my regex variable as the argument.

--- script.js
+++ script.js
@@ -12,8 +12,18 @@ function cleanInputString(str) {
 }
 
 function isInvalidInput(str){
-  const regex = /\d+e\d+/i;
+  const regex = /\d+e\d+/i; //"regex" is the variable name. The actual regex is /\d+e\d+/i, where i is 
+  //insensitive to capitalizations.
+  return str.match(regex);
 }
 
-// There is a shorthand character class to match any digit \d. I replaced my [0-9] character classes with
-// this shorthand. The + modifier needed to stay to keep matching my digit pattern one or more times.
+// Strings have a .match() method, which takes a regex argument. .match() will return an array of match results
+// - containing either the first match, or all matches if the global flag is used.
+// Example:
+// const str = 'example string';
+// const regex = /example/;
+// const result = str.match(regex);   //Returns ['example']. But idk why const is on this line? It doesn't
+    // need to be in my code.
+
+// In my code, I returned the result of calling the .match() method on str and passed my regex variable as
+// the argument. I'll use this match result later on.
