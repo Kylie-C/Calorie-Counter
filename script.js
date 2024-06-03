369d1766e4badae51b6775ded653eda53f2b8408
Kylie <kylie.conner93@gmail.com>
2024-06-03 16:40:19 -0400

    Testing isInvalidInput(){} function with the argument 10.

--- script.js
+++ script.js
@@ -16,11 +16,17 @@ function isInvalidInput(str){
   return str.match(regex);
 }
 
-console.log(isInvalidInput("1e3"))
+console.log(isInvalidInput("10"))
 
-// Testing my isInvalidInput function. I want to check if the function can detect scientific notation like 
-// 1e3 or 10e2. While this is a valid way to represent numbers, it's not a valid input for my calorie
-// counter project. 
-// The console.log() must go outside and below the function itself to test it.
+// The former console output showed as ['1e3, index: 0, input: '1e3', groups: undefined]
+// '1e3' is the matched value against the /\d+e\d+/i regex.
+// index:0 is the index of the matched value in the string (it's the first thing in the index).
+// input: '1e3' is the original string that was matched.
+// groups: undefined are the matches groups, which are not used in this case. I'll learn more about groups in 
+    // a later project.
 
-// The console output shows as ['1e3, index: 0, input: '1e3', groups: undefined]
+// Now I'm testing for a valid input, so I updated the console statement to the following.
+    // The console returns "null", which means no match is found. In this case, the isInvalidInput function 
+    // should return null when the input is a valid number WITHOUT any scientific notation.
+// Null in JS is a special primitive that represents the intentional absence of a value. In a boolean context,
+// null is considered falsy which evaluates to false in a conditional statement.
