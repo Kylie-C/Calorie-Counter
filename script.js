60af0ae14059832c2cd39c1e265cebf7c79a62bc
Kylie <kylie.conner93@gmail.com>
2024-06-03 16:41:16 -0400

    Removed the console.log

--- script.js
+++ script.js
@@ -16,17 +16,5 @@ function isInvalidInput(str){
   return str.match(regex);
 }
 
-console.log(isInvalidInput("10"))
 
-// The former console output showed as ['1e3, index: 0, input: '1e3', groups: undefined]
-// '1e3' is the matched value against the /\d+e\d+/i regex.
-// index:0 is the index of the matched value in the string (it's the first thing in the index).
-// input: '1e3' is the original string that was matched.
-// groups: undefined are the matches groups, which are not used in this case. I'll learn more about groups in 
-    // a later project.
-
-// Now I'm testing for a valid input, so I updated the console statement to the following.
-    // The console returns "null", which means no match is found. In this case, the isInvalidInput function 
-    // should return null when the input is a valid number WITHOUT any scientific notation.
-// Null in JS is a special primitive that represents the intentional absence of a value. In a boolean context,
-// null is considered falsy which evaluates to false in a conditional statement.
+// Finished testing the isInvalidInput(){} function, so I removed the console.log.
