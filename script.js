c87add79bbe9ef3bf8f2bc18fe80d15a7f62aa52
Kylie <kylie.conner93@gmail.com>
2024-06-04 10:02:29 -0400

    Cleaned up the function since I incorporated a template literal.

--- script.js
+++ script.js
@@ -17,21 +17,8 @@ function isInvalidInput(str){
 }
 
 function addEntry(){ 
-    const targetId = "#" + entryDropdown.value;
-    const targetInputContainer = document.querySelector(`${targetId} .input-container`);
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
 }
 
-// JS has a feature called TEMPLATE LITERALS, which allow you to interpolate variables directly within a 
-// string. Template literals are denoted with backticks ``, as opposed to single or double quotes. Varibales
-// can be passes in to a template literal by surrounding the variable with ${} (the value of the variable 
-// will be inserted into the string.)
-
-// Example:
-// const name = "Naomi";
-// const templateLiteral = `Hello, my name is ${name}~!`;
-// console.log(templateLiteral);
-
-// The console will show the string "Hello, my name is Naomi~!".
-
-// So, I replaced my concatenated string in the querySelector() with a template literal, making sure to keep
-// the space between my targetId variable and .input-container.
+// Thanks to template literals, you actually don't need the targetId variable at all. I removed that variable
+// and updated my template literal to replace targetId with entryDropdown.value and added the # before that.
