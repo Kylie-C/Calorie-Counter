27985d327a52907db4639b01fffb47db5146dde0
Kylie <kylie.conner93@gmail.com>
2024-06-04 09:24:52 -0400

    Declared targetId variable and assigned it # + entryDropdown.value

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
+// Here's where I queried it!
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
@@ -17,7 +18,13 @@ function isInvalidInput(str){
 }
 
 function addEntry(){ 
+    const targetId = "#" + entryDropdown.value;
 }
 
-// My next step is to allow users to add entries to the calorie counter, so I declared an empty function
-// with no parameters.
+// I need to know which category the entry goes in. Luckily, I added a dropdown for the user to select a 
+// category. Earlier, I queried (linked?) that dropdown earlier in your JS and assigned it to the 
+// entryDropdown button. I can use the .value property to get the value of the selected option.
+
+// I used concatenation to add a # to the beginning of the .value property of entryDropdown, and assigned
+// that result to a targetId variable.
+// Sing the "#" is a string, I imagine it will show as #USER-SELECTION (e.g., #Breakfast).
