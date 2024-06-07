2484ddd86365784774a9c14fd218a69141fd7e75
Kylie <kylie.conner93@gmail.com>
2024-06-07 15:14:37 -0400

    Added a 2nd input and reorganized.

--- script.js
+++ script.js
@@ -19,13 +19,25 @@ function isInvalidInput(str){
 function addEntry(){ 
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-        <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
+    const HTMLString = `
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input 
+            type="text" 
+            id="${entryDropdown.value}-${entryNumber}-name" 
+            placeholder="Name"
+        />
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>;
+        <input 
+            type="number" 
+            min="0" 
+            placeholder="Calories" 
+            id="${entryDropdown.value}-${entryNumber}-calories"
+        />`;
 }
 
 
-// I created another label element (on a new line) at the end of my HTMLString (again, note where the closing
-// backtick ` is for the template literal). This label should have the text "Entry # Calories", using your 
-// template literal syntax to replace # with the value of entryNumber, and the for attribute set to
-// X-#-calories, where X is the value of entryDropdown and # is the value of entryNumber.
+// On a new line after my 2nd label, I created another input element. I gave it a type attribute set to number
+// a min attribute set to 0 (to ensure negative calories cannot be added), a placeholder attribute set to 
+// Calories, and an id attribute that matches the for attribute of my 2nd label element.
+
+// Also moved the closing backtick of the first lavel element and reorganized.
