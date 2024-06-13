46abb50f2e8da7355d3fcf82fa7fbbde669b5082
Kylie <kylie.conner93@gmail.com>
2024-06-13 11:28:06 -0400

    Added currVal to the calories total.

--- script.js
+++ script.js
@@ -38,20 +38,30 @@ function addEntry(){
 
 function getCaloriesFromInputs(list){
     let calories = 0;
+
     for (const item of list) {
         const currVal = cleanInputString(item.value);
         const invalidInputMatch = isInvalidInput(currVal);
+
         if (invalidInputMatch) {
             alert(`Invalid Input: ${invalidInputMatch[0]}`)
             isError = true;
             return null;
-// In programming, null is meant to represet the absense of a value. In this case, if the user enters an
-// invalid input, you want to alert them and then return null to indicate that the function has failed.
-
-// Still within my if block, I set isError (which was previously set to false at the top) to true and returned
-// null. Since variables set within functions only exist within that function, that means in THIS function,
-// isError will equal true, but outside of this function, it will equal false.
         }
+
+        calories += Number(currVal);
+// Remember that return ends the execution of a function. After my if block, I needed to handle the logic for
+// when the input if valid. Because my if statement returns a value, I do not need an else statement.
+
+// I used the addition assignment operatoe += to add currVal to my calories total. I needed to use the Number
+// constructor to convert currVal to a number.
+
+// The Number constructor is a function that converts a value to a number. If the value cannot be converted, it 
+// returns NaN which stands for "Not a Number".
+
+// Example:
+//         Number('10');    // returns the number 10.
+//         Number('abc');   // returns NaN
     }
 }
 
